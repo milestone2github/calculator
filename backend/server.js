c35a353a9c531ad1a6aa88cd2616208a5e4b85dd
Kishan Kumar <singhkishan9911@gmail.com>
2024-12-27 16:18:48 +0530

    modify server to serve static frontend files

--- backend/server.js
+++ backend/server.js
@@ -16,19 +16,20 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
 // CORS configuration
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  // origin: (origin, callback) => {
+  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+  origin: true,
   credentials: true
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Middleware to provide db access
 function dbAccess(req, res, next) {
@@ -39,14 +40,14 @@ app.use(dbAccess);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/retirement-calculator', retirementCalcRoutes);
-app.get('/', (req, res) => {
-  res.send('Welcome to Calculator API');
-})
+// app.get('/', (req, res) => {
+//   res.send('Welcome to Calculator API');
+// })
 
 // wildcard route to serve static files from frontend 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
